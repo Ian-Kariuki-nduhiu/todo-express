8d7cd96dc6955c8a50070e1d04c7248d8126803d
ian <nduhiu.ian65@gmail.com>
2026-02-16 09:29:12 +0300

    complete express + mongodb crud

--- index.js
+++ index.js
@@ -2,14 +2,27 @@ import express from "express";
 import fs from "fs";
 import { fileURLToPath } from "url";
 import path from "path";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import Todo from "./models/Todo.js";
+
+dotenv.config();
 
 const app = express();
 
 const PORT = 3000;
 
 //middlewares, run before routes
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+
+//connecting to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("successfully connected to database"))
+  .catch((err) => console.error("Mongodb error: ", err));
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -20,6 +33,8 @@ if (!fs.existsSync(DATA_FILE)) {
 }
 
 //routes
+// /api/..  --> use file storage
+// /app/.. --> use database storage
 app.get("/api/todos", (req, res) => {
   const todos = JSON.parse(fs.readFileSync(DATA_FILE, "utf8"));
   res.json(todos);
@@ -102,4 +117,50 @@ app.delete("/api/todos/:id", (req, res) => {
 
 app.listen(PORT, () => {
   console.log("Server running at port: 3000");
+});
+
+//endpoints , use mongodb for storage and fetching
+app.get("/app/todos", async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (error) {
+    res.status(500).json({ error: `Error fetching todos: ${error}` });
+  }
+});
+
+app.post("/app/todos", async (req, res) => {
+  try {
+    const newTodo = new Todo({
+      text: req.body.text,
+    });
+    await newTodo.save();
+    res.status(201).json(newTodo);
+  } catch (error) {
+    res.status(500).json({ error: `Error adding todo: ${error}` });
+  }
+});
+
+app.put("/app/todos/:id", async (req, res) => {
+  try {
+    const todo = await Todo.findByIdAndUpdate(
+      req.params.id,
+      { completed: req.body.completed },
+      { returnDocument: "after" },
+    );
+    if (!todo) return res.status(404).json({ error: "Todo not found" });
+    res.json(todo);
+  } catch (error) {
+    res.status(500).json({ error: `Error updating todo ${error}` });
+  }
+});
+
+app.delete("/app/todos/:id", async (req, res) => {
+  try {
+    const todo = await Todo.findByIdAndDelete(req.params.id);
+    if (!todo) return res.status(404).json({ error: "Todo not found" });
+    res.json(todo);
+  } catch (error) {
+    res.status(500).json({ error: `Error deleting todo ${error}` });
+  }
 });
